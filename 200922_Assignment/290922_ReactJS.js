23e23a2b256530b6ea2b6efc3b38a524d26f98bd
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-09-20 20:28:19 +0800

    initial commit

---
+++ 200922_Assignment/290922_ReactJS.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      color: 'red',
+    }}>
+
+    <h1> Welcome To My First React Application</h1>
+    </div>
+    
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
