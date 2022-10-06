8f55b35d35ffbc8a209ff226af7d735918b35670
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-06 16:54:29 +0800

    initial commit ArrayList <User>, Iteration, DateTime
    File Writing User --> Name & Address
    Create 10 Users in List
    Using Iteration, Write User Data into File along with Date & Time

---
+++ 061022_Java_Assignment/Names.java
@@ -0,0 +1,20 @@
+package FileWriting;
+
+public class Names {
+    private String Name;
+    private String Address;
+
+    public Names(String Name, String Address){
+        this.Name = Name;
+        this.Address = Address;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+}
