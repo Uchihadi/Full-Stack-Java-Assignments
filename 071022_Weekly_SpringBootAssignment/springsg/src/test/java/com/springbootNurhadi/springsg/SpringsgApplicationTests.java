b3a22cdf2f1ff91e7ea8a6df777e569ded9dd463
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-10 05:31:55 +0800

    initial commit Spring Boot

---
+++ 071022_Weekly_SpringBootAssignment/springsg/src/test/java/com/springbootNurhadi/springsg/SpringsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootNurhadi.springsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
