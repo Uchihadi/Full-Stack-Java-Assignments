04914e36e30d0aacb25519294b245f80ddab2050
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-10 05:24:34 +0800

    initial commit

---
+++ springsg/src/main/java/com/springbootNurhadi/springsg/Request/UserRequest.java
@@ -0,0 +1,21 @@
+package com.springbootNurhadi.springsg.Request;
+
+public class UserRequest {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    String email, password;
+}
