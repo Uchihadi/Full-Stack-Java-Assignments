59bc7a0f9104e1e6d9725dd9632959ebd5113930
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-14 00:19:33 +0800

    commit updated Spring Boot File

---
+++ 111022_SpringBootAssignment/springsg/src/main/java/com/springbootNurhadi/springsg/Request/APILoginRequest.java
@@ -0,0 +1,22 @@
+package com.springbootNurhadi.springsg.Request;
+
+public class APILoginRequest {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    String email;
+    String password;
+}
---
+++ 111022_SpringBootAssignment/springsg/src/main/java/com/springbootNurhadi/springsg/model/UserModel.java
@@ -0,0 +1,69 @@
+package com.springbootNurhadi.springsg.model;
+
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+// Default Operations for Database ---> CRUD; Not achievable without the POJO class
+// Create another Repository Layer; Each Model will require one Repository --> If you need for CRUD
+// Optional. When you want to execute any Queries you will need another Repo Layer
+
+@Entity
+@Table (name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) //Automatically generated strategy types
+    Integer id;
+    String name, email, mobile;
+
+    @JsonIgnore
+    String password;
+
+    public UserModel(Integer id, String name, String email, String password){
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
