c6be56a0de7c3e4de91f54dc4d73654f032337ee
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-17 01:47:54 +0800

    commit React

---
+++ 131022_Token_SpringBootAssignment/ReactJS/src/Module.js
@@ -0,0 +1,7 @@
+// Exporting Class Person to Module2.JS
+
+export class Person{
+    printMessage(){
+        console.log("Hello World");
+    }
+}
---
+++ 131022_Token_SpringBootAssignment/ReactJS/src/Module2.js
@@ -0,0 +1,9 @@
+//  ./ --> Current Directory
+// ../ --> One Back
+// ../../ --> Two Back
+
+// Importing Class Persion from Module.js
+import Person from './Module';
+
+let person = Person();
+person.printMessage();
