29d2ff40895ca83fd01a3650ed8e80db85164028
Nurhadi Abdul Rahman <112716660+Uchihadi@users.noreply.github.com>
2022-10-16 22:23:20 +0800

    commit 12112022 CRUD Spring Assignment

---
+++ 121022_CRUD_SpringBootAssignment/src/main/java/com/springbootNurhadi/springsg/Response/GeneralResponse.java
@@ -0,0 +1,44 @@
+package com.springbootNurhadi.springsg.Response;
+
+import com.springbootNurhadi.springsg.model.UserModel;
+
+public class GeneralResponse {
+    // Creates a general response for the entire application
+    // Add String --> Right Click Getter and Setter
+    String Message = "";
+    UserModel userModel = null;
+
+    public GeneralResponse() {
+
+    }
+
+    public GeneralResponse(UserModel userModel) {
+        this.userModel = userModel;
+    }
+
+
+    //    public String getMyName() {
+//
+//        return myName;
+//    }
+//
+//    public void setMyName(String myName) {
+//        this.myName = myName;
+//    }
+//
+//    String myName;
+    public GeneralResponse(String Message){ //POJO Class Message
+
+        this.Message = Message;
+    }
+
+    public String getMessage() { // Get Message --> String
+
+        return Message;
+    }
+
+    public void setMessage(String message) { //Set Message --> Void --> Argument String
+        Message = message;
+    }
+
+}
